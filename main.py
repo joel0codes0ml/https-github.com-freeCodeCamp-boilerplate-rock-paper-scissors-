f9ab0909b997450b4b2e9629c7e8928badd381a5
joelcodesalot <joelkabui98@gmail.com>
2025-10-18 11:40:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+# Run matches
+print(play(player, quincy, 1000))
+print(play(player, abbey, 1000))
+print(play(player, kris, 1000))
+print(play(player, mrugesh, 1000))
